92d768102bb648971f7cd5a62a0b5c29910f23ab
Siddharth Bhayana <sbhayana26@gmail.com>
2016-09-21 01:25:23 +0530

    Create Kangaroo.cpp

---
+++ Kangaroo.cpp
@@ -0,0 +1,35 @@
+//https://www.hackerrank.com/challenges/kangaroo
+
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+	{ int x1, v1, x2, v2;
+	cin>>x1>>v1>>x2>>v2;
+	int i;
+	for(i=0; INT_MAX; ++i)
+		{ if ((x2==x1) && (v2==v1))
+			{ cout<<"YES";
+			break;
+			}
+        else if((x1==x2) && (v1<v2))
+            { cout<<"NO";
+            break;
+            }
+        else if ((x1<=x2) && (v1<=v2))
+            { cout<<"NO";
+            break;
+            }
+        else
+            { if(((x1-x2)%(v2-v1))==0)
+    		    { cout<<"YES";
+			    break;
+			    }
+            else
+                { cout<<"NO";
+                break;
+                }
+	        }
+        }
+	return 0;
+	}
