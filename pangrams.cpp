9b8489554b5def7d99c6e245f37d94a3f2f67967
Siddharth Bhayana <sbhayana26@gmail.com>
2017-01-20 22:56:55 +0530

    Update pangrams.cpp

--- pangrams.cpp
+++ pangrams.cpp
@@ -1,2 +1,30 @@
+//Needs modification.
 // https://www.hackerrank.com/challenges/pangrams
 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+    { char lowerCase[] = "abcdefghijklmnopqrstuvwxyz";
+    char upperCase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char s[];
+    gets(s);
+    int i, j, k[26], k;
+    for(i=0; i<26; ++i)
+        { for(j=0; j<strlen(s); ++j)
+            { if(lowerCase[i]==s[j] || upperCase[i]==s[j])
+                k[i] = 1;
+            }
+        }
+    for(i=0; i<26; ++i)
+        { if(k[i]==0)
+            { k = 0;
+            break;
+            }
+        }
+    if(k==0)
+        cout<<"not pangram";
+    else
+        cout<<"pangram";
+    return 0;
+    }
